b514c62a68c06664510214c66a0c972c9605b195
Angela Campion <campion.angela@gmail.com>
2018-04-15 19:49:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,16 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+  index
 end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+move(board, 0, "X")
+
 puts display_board(board)
 
 puts "Where would you like to go? 1-9"
