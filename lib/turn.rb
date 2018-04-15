3ec4b9fa4c8e51d88aca60d46e1be8041f2558fc
Angela Campion <campion.angela@gmail.com>
2018-04-15 20:38:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,8 +7,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-puts display_board(board)
-
 def input_to_index(user_input)
   index = user_input.to_i - 1
   index
