1a8be3e543b8664988353504e120462337898c05
Angela Campion <campion.angela@gmail.com>
2018-04-15 20:25:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   input_to_index(user_input)
-  if valid_move?([" ", " ", " ", " ", " ", " ", " ", " ", " "], 0) == false
+  if valid_move?([" ", " ", " ", " ", " ", " ", " ", " ", " "], 1) == false
     puts "Please enter 1-9:"
-  end 
+  end
 end
